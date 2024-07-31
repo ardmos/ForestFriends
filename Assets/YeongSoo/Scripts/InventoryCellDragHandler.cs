caf074a29d6efe8291824dc0eb9e38b5ca4d1d9a
ardmos <46891528+ardmos@users.noreply.github.com>
2024-08-01 00:18:31 +0900

    feat (FF-133) : 아이템이 비어있는 곳으로 위치 이동시 기존 셀에 있던 아이템 정보 제거 기능 구현

--- Assets/YeongSoo/Scripts/InventoryCellDragHandler.cs
+++ Assets/YeongSoo/Scripts/InventoryCellDragHandler.cs
@@ -7,18 +7,22 @@ public class InventoryCellDragHandler : MonoBehaviour
 {
     public InventoryCell inventoryCell;
 
-    // 드래그된 아이템을 현재 셀에 배치
+    // 비어있는 셀에 아이템이 배치될 때
     public void OnDrop(InventoryItem draggedItem)
     {
+        // 기존 셀에 있는 occupied아이템 정보 제거
+        Inventory.instance.GetInventoryCellByPos(draggedItem.GetItemData().currentCellPos).occupyingItem = null;
+
+        // 드래그된 아이템을 현재 셀에 배치
         SetItem(draggedItem);
     }
 
-    // 현 아이템과 draggedItem의 위치를 교환한다
+    // 드래그된 셀에 기존에 아이템이 존재할 때. 현 아이템과 draggedItem의 위치를 교환합니다
     public void OnSwapItems(InventoryItem draggedItem)
     {
         // draggedItem의 기존 위치에 현 아이템을 배치시킨다
         InventoryCellDragHandler draggedItemDragHandler = Inventory.instance.GetInventoryCellByPos(draggedItem.GetItemData().currentCellPos).inventoryCellDragHandler;
-        draggedItemDragHandler.OnDrop(inventoryCell.occupyingItem);
+        draggedItemDragHandler.SetItem(inventoryCell.occupyingItem);
 
         // 현 셀에 draggedItem을 배치시킨다
         SetItem(draggedItem);
